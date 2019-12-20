e8b2512bb5a9de2a96cf29432e6e6086c051497a
Dalai Felinto <dfelinto@gmail.com>
2019-12-20 19:35:56 +0100

    Update bug count

--- bug-count.sh
+++ bug-count.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Bugs: Status: Confirmed, Subtype: Bug, WITHOUT #tracker_curfew
+maniphest_count="$HOME/src/phabricator-maniphest-count/maniphest-count.sh"
+SAMPLES="$HOME/src/phabricator-maniphest-count/samples"
 
+open_bugs="$SAMPLES/open-bugs.json"
+tracker_curfew="$SAMPLES/tracker-curfew.json"
+old_open_confirmed_bugs="$SAMPLES/old-open-confirmed-bugs.json"
 
-maniphest_count="/home/dfelinto/src/tools/dailies/maniphest-count.sh"
-open_bugs="/home/dfelinto/src/tools/dailies/samples/open-bugs.json"
-tracker_curfew="/home/dfelinto/src/tools/dailies/samples/tracker-curfew.json"
+# old approach
+#$maniphest_count "$old_open_confirmed_bugs" 2>/dev/null
+$maniphest_count "$SAMPLES/test.json"
 
-
-$maniphest_count $open_bugs $tracker_curfew 2>/dev/null
+# new approach
+#$maniphest_count "$open_bugs" "$tracker_curfew" 2>/dev/null
